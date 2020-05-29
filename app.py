475165b342d5cba6ce8e92209a7b959bf8fd1117
Bendik Wang Andreassen <bendik.wang.andreassen@finn.no>
2020-05-29 09:17:55 +0200

    Readable variable name

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ def headers():
     return str_headers
 
 
-def md5(fname):
+def md5(file_name):
     hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
+    with open(file_name, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
