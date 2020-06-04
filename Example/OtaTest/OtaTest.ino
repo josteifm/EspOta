c0a120ce13b44256d2b5e4692568391c86a94df8
jostein malde <jostein.malde@gmail.com>
2020-06-04 21:42:57 +0200

    Add callbacks to Arduino example

--- Example/OtaTest/OtaTest.ino
+++ Example/OtaTest/OtaTest.ino
@@ -8,8 +8,31 @@ const char* ssid = "intertubes";
 const char* password = "hesterbest";
 const char* host = "OTA-TEST";
 
+void update_started() {
+  Serial.println("CALLBACK:  HTTP update process started");
+}
+
+void update_finished() {
+  Serial.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total) {
+  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err) {
+  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+
+
 void checkForUpdate(){
-   t_httpUpdate_return ret = ESPhttpUpdate.update("10.0.0.30", 54321, "/file", "optional current version string here");
+
+  ESPhttpUpdate.onStart(update_started);
+  ESPhttpUpdate.onEnd(update_finished);
+  ESPhttpUpdate.onProgress(update_progress);
+  ESPhttpUpdate.onError(update_error);
+
+  t_httpUpdate_return ret = ESPhttpUpdate.update("10.0.0.30", 54321, "/file", "optional current version string here");
   switch(ret) {
       case HTTP_UPDATE_FAILED:
           Serial.println("[update] Update failed.");
@@ -24,7 +47,7 @@ void checkForUpdate(){
 }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   /* switch on led */
   pinMode(LED_BUILTIN, OUTPUT);
@@ -49,14 +72,20 @@ void setup() {
   Serial.println("Ready");
 
   checkForUpdate();
+
+
+  String wakeReason = ESP.getResetReason();
+  Serial.println(wakeReason);
+
+  ESP.deepSleep(10);  
 }
 
 // the loop function runs over and over again forever
 void loop() {
 
  
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+  // digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // delay(100);                       // wait for a second
+  // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  // delay(100);                       // wait for a second
 }
